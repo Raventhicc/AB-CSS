ee3fbd111c63228283053a3e3eb10d9c0a8d99ad
Dattebayo13 <anonymous.2003.69@gmail.com>
2022-01-26 00:15:54 +0530

    Update AB Links.js

--- AB Links.js
+++ AB Links.js
@@ -19,6 +19,7 @@
   let header = document.querySelectorAll('h3 a');
   header = header[header.length - 1];
   let seriesName = document.querySelectorAll('h2 a')[0].innerText;
+  let seriesAnidb = document.querySelectorAll('h3 a')[0];
 
   document.querySelectorAll('h3 a').forEach(e => {
 
@@ -43,10 +44,26 @@
 
   };
 
-  
-  
+    function createnewLink2(linku, link1, link2, name, blank){
+   linku = document.createElement("a");
+	linku.innerHTML = " | " + name;
+	linku.href = link1 + seriesAnidb + link2 ;
+  linku.style.fontWeight = "900";
+  linku.style.fontSize = "15px";
+  //anilistLink.style.color = "#7FFFD4"
+    if(blank){
+
+        $(linku).attr('target', '_blank');
+
+    }
+	$(linku).insertAfter(header);
+
+  };
+
+
+
   createnewLink("nyaalink","https://nyaa.si/?f=0&c=0_0&q=","Nyaa",true);
-  createnewLink("u2link","https://u2.dmhy.org/torrents.php?incldead=1&spstate=0&inclbookmarked=0&search=","U2",true);
+  createnewLink2("u2link","https://u2.dmhy.org/torrents.php?incldead=1&spstate=0&inclbookmarked=0&search=","&search_area=4&search_mode=2","U2",true);
   createnewLink("tvDBlink","https://www.thetvdb.com/search?query=","TVDB",true);
   createnewLink("IMDBlink","https://www.imdb.com/find?q=","IMDB",true);
   createnewLink("anilistLink","https://anilist.co/search/anime?search=","Anilist",true);
