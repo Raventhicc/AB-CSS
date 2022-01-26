835d96b347be0705fcbec364272a0bf71edab274
Raventric <78981416+Raventhicc@users.noreply.github.com>
2022-01-26 15:13:57 +0530

    update
    Highlights - added more groups with reference to Seadex
    AB Links - Same as Dattebayo's with addition of Sonarr link
    ABsense - Absinth's CSS (only part of it that I want)

--- AB Links.js
+++ AB Links.js
@@ -64,6 +64,7 @@
 
   createnewLink("nyaalink","https://nyaa.si/?f=0&c=0_0&q=","Nyaa",true);
   createnewLink2("u2link","https://u2.dmhy.org/torrents.php?incldead=1&spstate=0&inclbookmarked=0&search=","&search_area=4&search_mode=2","U2",true);
+  createnewLink("Sonarr","http://localhost:9090/add/new?term=","Sonarr",true);
   createnewLink("tvDBlink","https://www.thetvdb.com/search?query=","TVDB",true);
   createnewLink("IMDBlink","https://www.imdb.com/find?q=","IMDB",true);
   createnewLink("anilistLink","https://anilist.co/search/anime?search=","Anilist",true);
